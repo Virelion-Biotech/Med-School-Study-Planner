cee4392ae87544f489693f98748e18a1f61bce1b
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-24 18:20:47 +0300

    feat: expose adaptive planner public interfaces

--- planner/__init__.py
+++ planner/__init__.py
@@ -1,9 +1,11 @@
 from .models import *
 from .scheduler import allocate_day, generate_week
 from .adaptive_optimizer import generate_adaptive_week, rank_actions
+from .adaptive_cpsat import AdaptivePlan, optimize_adaptive_week
 from .curriculum import CurriculumGraph
 from .fsrs import FSRSAdapter
 from .mastery import BKTParameters, predict_mastery, update_bkt
+from .readiness import ReadinessComponents, readiness_from_signals
 from .state import *
 from .utility import UtilityBreakdown, UtilityWeights, action_utility
 from .workload import initial_workload, update_workload
